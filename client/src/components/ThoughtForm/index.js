aac7a3da5afb86049a0f1b5f7245e59fce89369e
Jaewoo Han <jaewoohan@Jaewoos-Air.home>
2023-09-07 19:08:50 -0700

    add image upload feature

--- client/src/components/ThoughtForm/index.js
+++ client/src/components/ThoughtForm/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from 'react';
 
 const ThoughtForm = () => {
   const [formState, setFormState] = useState({
@@ -6,6 +6,7 @@ const ThoughtForm = () => {
     thought: "",
   });
   const [characterCount, setCharacterCount] = useState(0);
+  const fileInput = useRef(null);
 
   // update state based on form input changes
   const handleChange = (event) => {
@@ -19,9 +20,45 @@ const ThoughtForm = () => {
   const handleFormSubmit = (event) => {
     event.preventDefault();
 
+    const postData = async () => {
+      const res = await fetch('/api/users', {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formState),
+      });
+      const data = await res.json();
+      console.log(data);
+    };
+    postData();
+
     // clear form value
     setFormState({ username: "", thought: "" });
     setCharacterCount(0);
+  };
+
+  const handleImageUpload = (event) => {
+    event.preventDefault();
+    const data = new FormData();
+    data.append('image', fileInput.current.files[0]);
+    const postImage = async () => {
+      try {
+        const res = await fetch('/api/image-upload', {
+          mode: 'cors',
+          method: 'POST',
+          body: data,
+        });
+        if (!res.ok) throw new Error(res.statusText);
+        const postResponse = await res.json();
+        setFormState({ ...formState, image: postResponse.Location });
+        return postResponse.Location;
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    postImage();
   };
 
   return (
@@ -47,6 +84,13 @@ const ThoughtForm = () => {
           className="form-input col-12 "
           onChange={handleChange}
         ></textarea>
+        <label className="form-input col-12  p-1">
+          Add an image to your thought:
+          <input type="file" ref="{fileInput}" className="form-input p-2" />
+          <button className="btn" onClick="{handleImageUpload}" type="submit">
+            Upload
+          </button>
+        </label>
         <button className="btn col-12 " type="submit">
           Submit
         </button>
---
+++ server/utils/params-config.js
@@ -0,0 +1,14 @@
+const { v4: uuidv4 } = require('uuid');
+
+const params = (fileName) => {
+    const myFile = fileName.originalname.split('.');
+    const fileType = myFile[myFile.length - 1];
+    const imageParams = {
+      Bucket: 'deep_thoughts_bucket',
+      Key: `${uuidv4()}.${fileType}`,
+      Body: fileName.buffer,
+    };
+    return imageParams;
+  };
+
+  module.exports = params;
