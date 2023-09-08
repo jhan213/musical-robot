2b54a5ae923e5634c4a27639a11859800f8296f2
Jaewoo Han <jaewoohan@Jaewoos-Air.home>
2023-09-07 17:40:39 -0700

    add API routes to get all thoughts, all thoughts from a specified user, and to create a new user

---
+++ server/routes/user-routes.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const router = express.Router();
+
+const AWS = require('aws-sdk');
+const awsConfig = {
+  region: 'us-east-2',
+};
+AWS.config.update(awsConfig);
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+const table = 'Thoughts';
+
+router.get('/users', (req, res) => {
+    const params = {
+      TableName: table,
+    };
+    // Scan return all items in the table
+    dynamodb.scan(params, (err, data) => {
+      if (err) {
+        res.status(500).json(err); // an error occurred
+      } else {
+        res.json(data.Items);
+      }
+    });
+  });
+
+  module.exports = router;
