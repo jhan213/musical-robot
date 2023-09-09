5f2daee7c9254805afbf2ec9735ce2c42b875ddf
Jaewoo Han <jaewoohan@Jaewoos-Air.home>
2023-09-08 21:38:17 -0700

    change file path in LoadThoughts.js

--- server/db/LoadThoughts.js
+++ server/db/LoadThoughts.js
@@ -9,7 +9,7 @@ const dynamodb = new AWS.DynamoDB.DocumentClient({
 });
 console.log('Importing thoughts into DynamoDB. Please wait.');
 const allUsers = JSON.parse(
-  fs.readFileSync('./server/seed/users.json', 'utf8'),
+  fs.readFileSync('../seed/users.json', 'utf8'),
 );
 
 allUsers.forEach(user => {
